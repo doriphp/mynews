643c70784ba11b9865f79d7534e6f3c1119b86bb
Ochi_Takashi <rsp3892@gmail.com>
2020-01-08 08:02:48 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::post('news/create', 'Admin\NewsController@create');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); // 追記
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); // 追記
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
 
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit','Admin\ProfileController@edit');
-    Route::post('profile/create','Admin\ProfileController@create');
-    Route::post('profile/edit','Admin\ProfileController@update');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/create','Admin\ProfileController@create')->middleware('auth');
+    Route::post('profile/edit','Admin\ProfileController@update')->middleware('auth');
 });
 
 // 課題3
