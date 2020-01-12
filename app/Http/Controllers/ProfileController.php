85a7ebd4de78acd1e6479fdd5f61327e27563dfb
Ochi_Takashi <rsp3892@gmail.com>
2020-01-12 03:30:23 +0000

    first commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\HTML;
+use App\Profile;
+
+class ProfileController extends Controller
+{
+    public function index(Request $request){
+        
+        $profiles=profile::find($request->id);
+        if(empty($profiles)){
+            abort(404);
+        }
+        return view('profile.index', ['profiles' => $profiles]);
+    }
+}
---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.front_profile') 
+
+@section('profile','プロフィール表示ページ')
+
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <h2>マイプロフィール</h2>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row">
+                <table class="table table-dark">
+                    <thead>
+                        <tr>
+                            <th width="10%">ID</th>
+                            <th width="20%">氏名</th>                                
+                            <th width="10%">性別</th>
+                            <th width="20%">趣味</th>
+                            <th width="30%">自己紹介</th>
+                        </tr>
+                    </thead>
+                                    
+                    <tbody>
+                            <tr>
+                                <td>{{ $profiles->id }}</td>
+                                <td>{{ $profiles->name }}</td>
+                                <td>{{ $profiles->gender }}</td>
+                                <td>{{ \Str::limit($profiles->hobby,150) }}</td>
+                                <td>{{ \Str::limit($profiles->introduction,250) }}</td>
+                            </tr>
+                    </tbody>
+               </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
